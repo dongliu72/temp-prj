4d1344c8d239dccc436c111aad158545ccc4c247
dong@opulinks.com <dong@opulinks.com>
2018-12-07 15:52:47 +0800

    github submit code forMP v1.3

---
+++ SDK/APS_PATCH/middleware/netlink/ps_task/ps_patch.h
@@ -0,0 +1,84 @@
+/******************************************************************************
+*  Copyright 2017 - 2018, Opulinks Technology Ltd.
+*  ---------------------------------------------------------------------------
+*  Statement:
+*  ----------
+*  This software is protected by Copyright and the information contained
+*  herein is confidential. The software may not be copied and the information
+*  contained herein may not be used or disclosed except with the written
+*  permission of Opulinks Technology Ltd. (C) 2018
+******************************************************************************/
+
+#ifndef __PS_PATCH_H__
+#define __PS_PATCH_H__
+
+#include "ps.h"
+#include "ps_task.h"
+#include "ps_public.h"
+
+#include "cmsis_os_patch.h"
+#include "hal_system.h"
+#include "hal_vic.h"
+
+/******************** Constant Value Definition ********************/
+
+#define PS_OS_TASK_TRIGGER_PRECISE           1
+
+#define SOC_TICK_32K                         30.517578125
+
+/******************** Data Struct Declaration ********************/
+
+typedef struct
+{
+	uint32_t last_status;
+
+	t_ps_proc_ctrl proc_ctrl;
+	t_ps_busy_flag busy_flag;
+	t_ps_snap_time snap_time;
+
+	uint64_t wakeup_time;
+	uint32_t sleep_min_limit;
+	uint32_t wakeup_cost_gain;
+	uint32_t wakeup_cost;
+	uint8_t wakeup_cost_precise;
+
+	uint8_t app_apply_sleep;
+	uint8_t app_active_sleep;
+	
+	// patch for public
+	uint8_t xtal_ready_cost;
+	uint8_t app_apply_deep;
+
+} t_ps_conf_patch;
+
+/******************** Function Prototype ********************/
+
+void ps_patch_init(void);
+
+// ps - public
+void ps_init_patch(void);
+void ps_wait_xtal_ready_patch(void);
+int ps_sleep_patch(void);
+void ps_update_boot_gpio_int_status(uint32_t gpio_status);
+void ps_set_wakeup_callback_internal(PS_WAKEUP_CALLBACK callback);
+int  ps_is_smart_sleep_enabled(void);
+void ps_dbg_cmd_conf_io(char *io_p);
+void ps_parse_command_patch(char *pbuf, int len);
+
+// ps - private
+uint32_t ps_synchronize_timers_patch(void);
+void ps_default_io_callback(E_GpioIdx_t eIdx);
+void ps_default_wakeup_callback_patch(PS_WAKEUP_TYPE wakeup_type);
+
+// ps - for public API
+void ps_config_wakeup_ext_io(E_GpioIdx_t num, int enable, E_ItrType_t type, int invert, T_Gpio_CallBack callback);
+void ps_config_wakeup_callback(PS_WAKEUP_CALLBACK callback);
+void ps_enable_deep_sleep(void);
+void ps_enable_timer_sleep(uint32_t sleep_duration_us);
+void ps_enable_smart_sleep(int is_enable);
+
+// ps_task
+void ps_task_create_patch(void);
+void ps_task_patch(void *argument);
+
+#endif
---
+++ SDK/APS_PATCH/middleware/third_party/lwip-2.0.3/ports/rom_if/lwip_jmptbl_patch.c
@@ -0,0 +1,56 @@
+/******************************************************************************
+*  Copyright 2017 - 2018, Opulinks Technology Ltd.
+*  ---------------------------------------------------------------------------
+*  Statement:
+*  ----------
+*  This software is protected by Copyright and the information contained
+*  herein is confidential. The software may not be copied and the information
+*  contained herein may not be used or disclosed except with the written
+*  permission of Opulinks Technology Ltd. (C) 2018
+******************************************************************************/
+
+#include "lwip/opt.h"
+#include "sys_os_config_patch.h"
+
+/* user api related */
+extern void lwip_load_interface_socket_patch(void);
+
+/* ipv4 related */
+extern void lwip_load_interface_dhcp_patch(void);
+/* ipv6 related */
+
+
+/* common */
+extern void lwip_load_interface_tcpip_patch(void);
+extern void lwip_load_interface_timeouts_patch(void);
+/* network interface */
+
+/* network config */
+extern void lwip_load_interface_network_config_patch(void);
+
+/* porting layer */
+extern void lwip_load_interface_wlannetif_patch(void);
+extern void lwip_load_interface_sys_arch_freertos_patch(void);
+
+/* application && cli */
+extern void lwip_load_interface_lwip_helper_patch(void);
+extern void lwip_load_interface_cli_patch(void);
+
+/* tcpip if */
+extern void lwip_load_interface_tcpip_if_patch(void);
+
+void lwip_module_interface_init_patch(void)
+{
+    lwip_load_interface_timeouts_patch();
+    lwip_load_interface_tcpip_patch();
+    lwip_load_interface_socket_patch();
+    lwip_load_interface_wlannetif_patch();
+    lwip_load_interface_network_config_patch();
+    lwip_load_interface_lwip_helper_patch();
+    lwip_load_interface_cli_patch();
+    lwip_load_interface_tcpip_if_patch();
+    lwip_load_interface_dhcp_patch();
+    lwip_load_interface_sys_arch_freertos_patch();
+    
+    return;
+}
